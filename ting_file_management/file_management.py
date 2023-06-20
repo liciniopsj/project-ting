2ace59a609371b366940ca0cd02c2a720c0a873b
liciniopsj <licinio.psj@gmail.com>
2023-06-20 10:15:11 -0300

    req 2 - txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,22 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+from pathlib import Path
+
+def txt_importer(file_path):
+    path = Path(file_path)
+    
+    if not file_path.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+        return []
+
+    if not path.exists():
+        sys.stderr.write(f"Arquivo {file_path} não encontrado\n")
+        return []
+
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            lines = file.read().splitlines()
+        return lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {file_path} não encontrado\n")
+        return []
+    
