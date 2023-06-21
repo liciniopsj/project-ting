8308c818ebc0258fc1894470bee12c9307851633
liciniopsj <licinio.psj@gmail.com>
2023-06-20 21:28:39 -0300

    req 3 - file process - flaked

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,37 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def process(file_path, instance):
+    for entry in instance.queue:
+        if entry['nome_do_arquivo'] == file_path:
+            return
+
+    lines = txt_importer(file_path)
+    new_entry = {
+        "nome_do_arquivo": file_path,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+    instance.enqueue(new_entry)
+
+    processed_entry = instance.queue[-1]
+    print(processed_entry)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not len(instance):
+        print("Não há elementos")
+        return
+
+    path_file = instance.search(0)["nome_do_arquivo"]
+    instance.dequeue()
+    print(f"Arquivo {path_file} removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        data = instance.search(position)
+        print(data)
+    except IndexError:
+        sys.stderr.write("Posição inválida\n")
