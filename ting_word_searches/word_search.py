e62edc10782772425b871b58d38219fe28102174
liciniopsj <licinio.psj@gmail.com>
2023-06-20 22:56:25 -0300

    req 7 flaked

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,39 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found = []
+
+    for item in range(len(instance)):
+        occurrences = []
+
+        for line_index, line_content in enumerate(instance.search(item)['linhas_do_arquivo']):
+            if word.casefold() in line_content.casefold():
+                occurrences.append({'linha': line_index + 1})
+
+        if occurrences:
+            found.append({
+                'palavra': word,
+                'arquivo': instance.search(item)['nome_do_arquivo'],
+                'ocorrencias': occurrences,
+            })
+
+    return found
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for item in range(len(instance)):
+        occurrences = []
+
+        for line_index, line_content in enumerate(instance.search(item)['linhas_do_arquivo']):
+            if word.casefold() in line_content.casefold():
+                occurrences.append({'linha': line_index + 1, 'conteudo': line_content})
+
+        if occurrences:
+            results.append({
+                'palavra': word,
+                'arquivo': instance.search(item)['nome_do_arquivo'],
+                'ocorrencias': occurrences,
+            })
+
+    return results
+
